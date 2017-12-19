e622962ba8ab6c5cfe437706428bba185bc44da7
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-12-19 11:27:42 -0600

    update_chapter

---
+++ Chapter 8 - Tutorial 1/Chapter 8 - Tutorial 1/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFirst = New System.Windows.Forms.Label()
+        Me.lblSecond = New System.Windows.Forms.Label()
+        Me.lblThird = New System.Windows.Forms.Label()
+        Me.lblFourth = New System.Windows.Forms.Label()
+        Me.lblFifth = New System.Windows.Forms.Label()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblFirst
+        '
+        Me.lblFirst.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFirst.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFirst.Location = New System.Drawing.Point(34, 73)
+        Me.lblFirst.Name = "lblFirst"
+        Me.lblFirst.Size = New System.Drawing.Size(48, 40)
+        Me.lblFirst.TabIndex = 0
+        Me.lblFirst.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSecond
+        '
+        Me.lblSecond.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSecond.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSecond.Location = New System.Drawing.Point(112, 73)
+        Me.lblSecond.Name = "lblSecond"
+        Me.lblSecond.Size = New System.Drawing.Size(48, 40)
+        Me.lblSecond.TabIndex = 1
+        Me.lblSecond.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblThird
+        '
+        Me.lblThird.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblThird.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblThird.Location = New System.Drawing.Point(190, 73)
+        Me.lblThird.Name = "lblThird"
+        Me.lblThird.Size = New System.Drawing.Size(48, 40)
+        Me.lblThird.TabIndex = 2
+        Me.lblThird.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFourth
+        '
+        Me.lblFourth.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFourth.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFourth.Location = New System.Drawing.Point(264, 73)
+        Me.lblFourth.Name = "lblFourth"
+        Me.lblFourth.Size = New System.Drawing.Size(51, 40)
+        Me.lblFourth.TabIndex = 3
+        Me.lblFourth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblFifth
+        '
+        Me.lblFifth.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFifth.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFifth.Location = New System.Drawing.Point(348, 73)
+        Me.lblFifth.Name = "lblFifth"
+        Me.lblFifth.Size = New System.Drawing.Size(51, 40)
+        Me.lblFifth.TabIndex = 4
+        Me.lblFifth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(81, 209)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(103, 36)
+        Me.btnGenerate.TabIndex = 5
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(249, 209)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(103, 36)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 313)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.lblFifth)
+        Me.Controls.Add(Me.lblFourth)
+        Me.Controls.Add(Me.lblThird)
+        Me.Controls.Add(Me.lblSecond)
+        Me.Controls.Add(Me.lblFirst)
+        Me.Name = "Form1"
+        Me.Text = "Lottery Number Generator"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblFirst As System.Windows.Forms.Label
+    Friend WithEvents lblSecond As System.Windows.Forms.Label
+    Friend WithEvents lblThird As System.Windows.Forms.Label
+    Friend WithEvents lblFourth As System.Windows.Forms.Label
+    Friend WithEvents lblFifth As System.Windows.Forms.Label
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Chapter 8 - Tutorial 1/Chapter 8 - Tutorial 1/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Const intMax_SUBSCRIPT As Integer = 4         ' The maximum subscript.
+        Dim intNumbers(intMax_SUBSCRIPT) As Integer   ' Array declaration.
+        Dim intCount As Integer                       ' Loop counter.
+
+        Dim rand As New Random
+
+        For intCount = 0 To intMax_SUBSCRIPT          ' Fill the array with random numbers.
+            intNumbers(intCount) = rand.Next(100)
+        Next
+
+        lblFirst.Text = intNumbers(0).ToString()      ' Display the array elements in labels.
+        lblSecond.Text = intNumbers(1).ToString()
+        lblThird.Text = intNumbers(2).ToString()
+        lblFourth.Text = intNumbers(3).ToString()
+        lblFifth.Text = intNumbers(4).ToString()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
