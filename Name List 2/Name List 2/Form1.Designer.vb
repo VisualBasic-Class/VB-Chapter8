ec4bcedd00790a2ecf7def304eba9aaab649add8
Chane Eckhardt <33033313+ceckhardt22@users.noreply.github.com>
2017-12-19 11:30:21 -0600

    update_chapter

---
+++ Name List 2/Name List 2/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstFriends = New System.Windows.Forms.ListBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnAddName = New System.Windows.Forms.Button()
+        Me.btnShowAll = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstFriends
+        '
+        Me.lstFriends.FormattingEnabled = True
+        Me.lstFriends.Location = New System.Drawing.Point(251, 89)
+        Me.lstFriends.Name = "lstFriends"
+        Me.lstFriends.Size = New System.Drawing.Size(120, 186)
+        Me.lstFriends.TabIndex = 0
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(33, 183)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'btnAddName
+        '
+        Me.btnAddName.Location = New System.Drawing.Point(50, 221)
+        Me.btnAddName.Name = "btnAddName"
+        Me.btnAddName.Size = New System.Drawing.Size(66, 26)
+        Me.btnAddName.TabIndex = 2
+        Me.btnAddName.Text = "Add Name"
+        Me.btnAddName.UseVisualStyleBackColor = True
+        '
+        'btnShowAll
+        '
+        Me.btnShowAll.Location = New System.Drawing.Point(280, 293)
+        Me.btnShowAll.Name = "btnShowAll"
+        Me.btnShowAll.Size = New System.Drawing.Size(66, 26)
+        Me.btnShowAll.TabIndex = 3
+        Me.btnShowAll.Text = "Show All"
+        Me.btnShowAll.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 397)
+        Me.Controls.Add(Me.btnShowAll)
+        Me.Controls.Add(Me.btnAddName)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lstFriends)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstFriends As System.Windows.Forms.ListBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents btnAddName As System.Windows.Forms.Button
+    Friend WithEvents btnShowAll As System.Windows.Forms.Button
+
+End Class
